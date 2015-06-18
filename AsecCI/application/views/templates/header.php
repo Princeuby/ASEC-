988a0ca8f755c976a679881319ffe6d68dafcfb5
BrainMaestro <ezimaestro@gmail.com>
2015-06-18 18:56:26 +0100

    Added asec logo

--- AsecCI/application/views/templates/header.php
+++ AsecCI/application/views/templates/header.php
@@ -5,6 +5,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="icon" href="<?php echo base_url('assets/images/logo.png'); ?>">
 		<link rel="stylesheet" href="<?php echo base_url('assets/css/main.css'); ?>">
 		<link rel="stylesheet" href="<?php echo base_url('assets/css/custom.css'); ?>">
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
