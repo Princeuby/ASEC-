54d2fa3925f73da30c21226885244701367a3c6e
BrainMaestro <ezimaestro@gmail.com>
2015-06-19 14:40:43 +0100

    Improved mobile view

--- AsecCI/application/views/index.php
+++ AsecCI/application/views/index.php
@@ -6,12 +6,12 @@
 					<hr><h1>Security Management System</h1><hr>
 				</header>
 
-				<section class="6u center">
+				<section class="6u center size-panel">
 					<img id="logo" src="<?php echo base_url('assets/images/logo.png'); ?>" />
 					<?php echo validation_errors(); ?>
 					<?php echo form_open('login') ?>
-					    <label class="red-box"><input type="text" name="id" placeholder="Officer ID"></label>
-					    <label class="red-box"><input type="password" name="password" placeholder="Password"></label><br>
+					    <label class="red-box"><input type="text" name="id" placeholder="Officer ID" class="size-input"></label>
+					    <label class="red-box"><input type="password" name="password" placeholder="Password" class="size-input"></label><br>
 					    <input type="submit" name="submit" value="Login">
 					</form>
 				</section>
