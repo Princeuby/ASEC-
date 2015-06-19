55d2eb0bb38387f6e335ae72fcf1d0de23cf2355
BrainMaestro <ezimaestro@gmail.com>
2015-06-19 22:14:46 +0100

    Fixed js links in footer

--- AsecCI/application/views/templates/footer.php
+++ AsecCI/application/views/templates/footer.php
@@ -6,13 +6,13 @@
 	</div>
 
 		<!-- Scripts -->
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js/jquery.scrolly.min.js"></script>
-	<script src="assets/js/jquery.scrollzer.min.js"></script>
-	<script src="assets/js/skel.min.js"></script>
-	<script src="assets/js/util.js"></script>
-	<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-	<script src="assets/js/main.js"></script>
+	<script src="<?php echo base_url('assets/js/jquery.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/js/jquery.scrolly.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/js/jquery.scrollzer.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/js/skel.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/js/util.js');?>"></script>
+	<!--[if lte IE 8]><script src="<?php echo base_url('assets/js/ie/respond.min.js');?>"></script><![endif]-->
+	<script src="<?php echo base_url('assets/js/main.js');?>"></script>
 
 	</body>
 </html>
