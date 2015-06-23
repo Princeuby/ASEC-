22eb779706874d2523de6fcacef0d1082e6b4e66
BrainMaestro <ezimaestro@gmail.com>
2015-06-23 13:20:14 +0100

    Create new activity report with previous officer id
    Also shows details for current activity report along with some clever
    element hiding

--- AsecCI/application/controllers/Officer.php
+++ AsecCI/application/controllers/Officer.php
@@ -2,8 +2,9 @@
 class Officer extends CI_Controller {
 	
 	public function __construct() {
-            parent::__construct();
-            $this->load->model('officer_model');
+        parent::__construct();
+        $this->load->model('officer_model');
+		$this->load->library('session');
     }
 	
 	public function index() {
@@ -15,7 +16,6 @@ class Officer extends CI_Controller {
 	}
 	
 	private function set_data($page='Home') { // sets the data variables to avoid repition
-		$this->load->library('session');
 		$data['title'] = 'Officer';
 	    $data['page'] = $page;
 		$data['name'] = $this->session->userdata('officerFullName');
@@ -34,18 +34,15 @@ class Officer extends CI_Controller {
 		$data = $this->set_data('Activity Report');
 		// Gets activity report for current shift
 		$data['report'] = $this->officer_model->get_activity_report($data['id']);
+		$data['display'] = 'None'; // Sets CSS display rule of create activity report form in view
+		$data['display_incident'] = 'block'; // Sets CSS display rule of new incident form in view
 		// Checks if activity report has been created
 		if (empty($data['report'])) {
 			// Creates new activity report **Uses wrong previous ID**
-			$data['report'] = $this->officer_model->create_activity_report(
-				$data['id'],$data['id']); 
-			$data['report'] = $this->officer_model->get_activity_report($data['id']);
+			$data['display'] = 'block'; // Makes new activity report form visible
+			$data['display_incident'] = 'None'; // Hides the new incident report form
 		}
-		
-		// $this->load->view('templates/header', $data);
-		// $this->load->view('templates/nav', $data);
-		// $this->load->view('officer/activity_report', $data);
-	    // $this->load->view('templates/footer');
+	
 		$this->load->helper('form');
 	    $this->load->library('form_validation');
 		
@@ -66,25 +63,18 @@ class Officer extends CI_Controller {
 		
 	}
 	
-	public function incident() {
-		$data = $this->set_data('Activity Report');
+	// Creates new activity report from form
+	public function new_activity_report() {
 		$this->load->helper('form');
 	    $this->load->library('form_validation');
+		$this->form_validation->set_rules('prevID', 'Username', 'required');
 		
-		$this->form_validation->set_rules('incident', 'Username', 'required');
-	    $this->form_validation->set_rules('details', 'Text', 'required');
-
-		$this->load->view('templates/header', $data);
-		$this->load->view('templates/nav', $data);
-			
-		if ($this->form_validation->run() === FALSE) {
-		    $this->load->view('officer/activity_report');
-	    }
-		else {
-	    	$this->load->view('officer/activity_success');
+		if ($this->form_validation->run() === TRUE) {
+			$previousOfficerID = $user = $this->input->post('prevID');
+			$officerID = $this->session->userdata('officerID');
+			$this->officer_model->create_activity_report($officerID,$previousOfficerID); 
 		}
-		
-	    $this->load->view('templates/footer');
+		$this->activity_report();		
 	}
 }	
 	
--- AsecCI/application/views/officer/activity_report.php
+++ AsecCI/application/views/officer/activity_report.php
@@ -1,15 +1,44 @@
-	    <span id="disp"></span>
-		<script>setInterval(clk, 100);</script>
+	    <style> /* I had to do this */
+			#new-report { display: <?php echo $display; ?>; }
+			.not-new {	display: <?php echo $display_incident; ?>; }
+		</style>
+		<!--<span id="disp"></span>-->
+		<!--<script>setInterval(clk, 100);</script>-->
 			<header>
 				<h2 class="alt">Your Activity <strong>Report</strong></h2>
-				<p>Don't  forget the small details that no one cares about</p>
 			</header>
-			<section class="6u center size-panel">
-				<?php echo form_open('officer/activity_report') ?>
-				    <label><input type="text" name="incident" placeholder="Incident" class="size-input"></label>
-				    <label><textarea name="details" placeholder="I found a missing dog" class="size-inpu"></textarea></label><br>
-				    <input type="submit" name="submit" value="Add">
-				</form>
+			<section class="8u center size-panel">
+				<section id="new-report">
+					<?php echo form_open('officer/new_activity_report') ?>
+					    <label><input type="text" name="prevID" placeholder="Previous Officer ID" class="size-input"></label>					
+						<input type="submit" name="submit" value="Create New Report">
+					</form>
+				</section>
+				<section class="not-new">
+					<span>Report Details</span>
+					<hr>
+					<table>
+						<tbody>
+							<tr>
+								<td>Report Created </td>
+								<td><?php echo $report['date_timeIn'];?></td>
+							</tr>
+							<tr>
+								<td>Previous Officer </td>
+								<td><?php echo $report['previous_officer_id'];?></td>
+							</tr>
+							<tr>
+								<td>Shift </td>
+								<td><?php echo $report['shift'];?></td>
+							</tr>
+						</tbody>
+					</table>
+					<?php echo form_open('officer/activity_report') ?>
+					    <label><input type="text" name="incident" placeholder="Incident" class="size-input"></label>
+					    <label><textarea name="details" placeholder="I found a missing dog" class="size-inpu"></textarea></label><br>
+					    <input type="submit" name="submit" value="Add Incident">
+					</form>
+				</section>
 			</section>
 			
 		</div>
