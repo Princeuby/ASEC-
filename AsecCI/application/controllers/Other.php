0af61746f1f8345f9e48c9e54c2e7d1fe1ff9a8d
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-06-26 15:15:17 +0100

    added 'view reports' to other ranks

--- AsecCI/application/controllers/Other.php
+++ AsecCI/application/controllers/Other.php
@@ -19,7 +19,7 @@ class Other extends Supervisor {
 
 	protected function set_data($page='Home') { // sets the data variables to avoid repition
 		$data = parent::set_data($page);
-		$data['functions'] = ['home', 'schedule', 'leaves', 'leave requests', 'activity report'];
+		$data['functions'] = ['home', 'schedule', 'activity report', 'view activity reports', 'leaves'];
 
 		return $data;
 	} 
