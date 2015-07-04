912cd1a52dd6c1706020fe222eb802932f12c154
BrainMaestro <ezimaestro@gmail.com>
2015-07-04 16:34:59 +0100

    Cleaned up create vacancy form

--- AsecCI/application/views/cso/vacancy.php
+++ AsecCI/application/views/cso/vacancy.php
@@ -2,30 +2,29 @@
 				<h2 class="alt">Create <strong>Vacancy</strong></h2>
 			</header>
 			<section class="center">
-				<?php echo "<span><h3>$failed_create</h3></span>"; ?>
-				<section id="createvacancy" class="4u 12u$(mobile) center">
-					<hr/>
+				<?php echo "<h3>$failed_create</h3>"; ?>
+				<section id="createvacancy" class="6u 12u$(mobile) center">
+					<hr>
 					<?php echo form_open("$designation/vacancy"); ?>
-						Position: <span><input type="text" name="vacant-position"></span>
-						Position Summary: <span><textarea name="vacant-summary"></textarea></span>
-						Department: <span><input type="text" name="vacant-department"></span>
-						Education Level: <span><select name="vacant-education-level" class='size-input'>
+						<label>Position: <input type="text" name="vacant-position" required></label>
+						<label>Position Summary: <textarea name="vacant-summary"></textarea></label>
+						<label>Department: <input type="text" name="vacant-department" required></label>
+						<label>Education Level: <br><select name="vacant-education-level" class='size-input' required>
 								<option value='' disabled selected>Choose:</option>
 								<option value='Primary'>Primary</option>
 								<option value='WAEC/JAMB/GCE'>WAEC/JAMB/GCE</option>
 								<option value='OND/HND/NCE'>OND/HND/NCE</option>
-								<option value='Bachelor Degree'>Bachelor Degree</option>
-								<option value='Master Degree'>Master Degree</option>
-								<option value='PHd'>PHd</option>
-							</select></span><br/><br/>
-						Working Experience: <span><textarea name="vacant-working-experience"></textarea></span>
-						Other Specifiations: <span><textarea name="vacant-other-specifications"></textarea></span>
-						<br/>Closing Date: <span><input type="date" name="vacant-closing-date" min="<?php echo date('Y-m-d'); ?>"></span>
-						<br/><br/><input name="Submit" type="submit" value="Create Vacancy" />
+								<option value='Bachelors Degree'>Bachelors Degree</option>
+								<option value='Masters Degree'>Masters Degree</option>
+								<option value='PhD'>PhD</option>
+							</select></label>
+						<label>Working Experience: <textarea name="vacant-working-experience"></textarea></label>
+						<label>Other Specifiations: <textarea name="vacant-other-specifications"></textarea></label>
+						<label>Closing Date: <input type="date" name="vacant-closing-date" min="<?php echo date('Y-m-d'); ?>"></label>
+						<input name="Submit" type="submit" value="Create Vacancy">
 					</form>
 				</section>
 			</section>
-
 		</div>
 	</section>
 </div>
