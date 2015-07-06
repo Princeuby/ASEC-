564b70adda831b9ba481e5c06584874abf9a5148
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-07-06 15:02:01 +0100

    added file upload function
    applicants can now upload their documents

--- AsecCI/application/views/vacancy/applicants.php
+++ AsecCI/application/views/vacancy/applicants.php
@@ -2,19 +2,19 @@
 				<h2 class="alt">Application Form: <strong><?php echo $position; ?></strong></h2>
 			</header>
 			<section class="center">
-				<p class="error"><?php echo $this->session->flashdata('failed_create'); ?></p>
-				<?php echo form_open("vacancy/add_application"); ?>
+				<p class="error"><?php echo $this->session->flashdata('failed_create'); //echo $error[0];?></p>
+				<?php echo form_open_multipart("vacancy/add_application"); ?>
 					<hr/>
-					<section class="4u 6u$(mobile) center">
-						First Name: <input name="applicant-firstName" type="text">
-						Middle Name: <input name="applicant-middleName" type="text">
-						Last Name: <input name="applicant-lastName" type="text">
-						Phone Number: <input name="applicant-phoneNumber" type="text" max="11">
-						Email: <input name="applicant-email" type="text">
+					<section class="5u 8u$(mobile) center">
+						First Name: <input class="" name="applicant-firstName" type="text">
+						Middle Name: <input class="size-input" name="applicant-middleName" type="text">
+						Last Name: <input class="size-input" name="applicant-lastName" type="text">
+						Phone Number: <input class="size-input" name="applicant-phoneNumber" type="text" max="11">
+						Email: <input class="size-input" name="applicant-email" type="email">
 						Application Letter: <br><em>Only word documents supported</em>
-							<input name="applicant-applicationLetter" type="file" accept="file_extension">
+							<input class="size-input" name="applicant-applicationLetter" type="file"><br>
 						Curriculum Vitae: <br><em>Only word documents supported</em>
-							<input name="applicant-curriculumVitae" type="file" accept="file_extension">
+							<input class="size-input" name="applicant-curriculumVitae" type="file">
 					</section>
 					<button name="applicant-apply" value="<?php echo $vacancyID; ?>"/>Apply</button>
 				<?php echo form_close(); ?>
